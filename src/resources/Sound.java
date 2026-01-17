54925ed7c0841deea1898d52bc7ac953afe6c053
Ilay Or-Chen <ilayorchen@gmail.com>
2026-01-18 00:12:33 +0200

    Implemented Sound class for the background music of the game

--- src/resources/Sound.java
+++ src/resources/Sound.java
@@ -1,4 +1,24 @@
 package resources;
 
+import javax.sound.sampled.*;
+import java.io.File;
+
 public class Sound {
+    private static Clip clip;
+
+    public static void playLoop(String path) {
+        try {
+            AudioInputStream audio = AudioSystem.getAudioInputStream(new File(path));
+            clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            System.out.println("Sound error");
+        }
+    }
+
+    public static void stop() {
+        if (clip != null) clip.stop();
+    }
 }
+
